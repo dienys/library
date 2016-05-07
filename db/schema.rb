81fd13d945d7e984eadd9dfc54e24110c7e1187a
malguesh <philippine.ramaherison@epitech.eu>
2016-05-07 13:18:21 +0000

    add column image to books

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505202321) do
+ActiveRecord::Schema.define(version: 20160507131526) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160505202321) do
     t.string   "status",      limit: 255
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+    t.string   "image_url",   limit: 255
   end
 
   create_table "loans", force: :cascade do |t|
